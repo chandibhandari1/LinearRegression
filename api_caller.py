93fb174ba1bf673e187e437178662e2c3da80c91
chandibhandari1 <49646654+chandibhandari1@users.noreply.github.com>
2020-06-26 18:24:36 -0400

    Delete api_caller.py

--- api_caller.py
+++
@@ -1,8 +0,0 @@
-import requests
-json_req ={'sales1': 245, 'sales2': 198, 'sales3': 431, 'sales4': 432,'deman_level':'high','best_seller':'A' }
-url = 'http://127.0.0.1:5000/pred'
-# # create the post request
-req = requests.post(url,json=json_req)
-print(req.status_code)
-# print the requested result
-print(req.json())
